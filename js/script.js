c8ef7a8339201c9b88ae2b13bba0b3a9dc4d64e7
NeoDalmatian <aviskovic100@gmail.com>
2023-05-28 21:14:28 +0200

    Add coloring functionality to the canvas
    -Add function color() that colors targeted "pixel".
    -Add callback function andMouseDown() whitch is checking if mouse is
    down (it is used in combination with event listener mousemove).
    -Add two event listeners to the "pixels" in canvasMaker(). First is
    mousedown, and second is mousemove with combination of andMouseDown().

--- js/script.js
+++ js/script.js
@@ -2,14 +2,27 @@ const canvas = document.querySelector(".canvas");
 
 canvasMaker();
 
+// Function that is checking if mouse is down (used with mousemove)
+function andMouseDown (e) {
+  if (e.buttons == 1) {
+    e.preventDefault();
+    color(e)
+  }
+}
+
+// Function that colors pixels
+function color (e) {
+  e.target.style.backgroundColor = "black";
+}
+
 // Creates "pixels" in canvas, parameter is number of pixels in row/column
 function canvasMaker (n = 16) {
-  // Determines real size of "pixels" in px, with 500 being dimension of canvas
-  let pixelSize = 500/n;
   // Creates, alters and appends "pixels" in canvas (number of them is n x n)
   for (let i = 0; i < (n * n); i++) {
     let pixel = document.createElement("div");
-    pixel.setAttribute("style", `width:${pixelSize}px; height:${pixelSize}px;`)
+    pixel.setAttribute("style", `width: ${500/n}px; height: ${500/n}px;`);
+    pixel.addEventListener("mousemove", andMouseDown);
+    pixel.addEventListener("mousedown", color)
     canvas.appendChild(pixel);
   }
 }
