b1442a23ff087432f1bbb7dab3a817d64e481225
NeoDalmatian <aviskovic100@gmail.com>
2023-05-29 14:08:48 +0200

    Implement functionality to canvas size button
    -Make function canvasEraser(), so when ever canvasMaker() is invoked it
    has to first earase canvas to draw the new one.
    -Select canvas size button and add event listener click, after being
    clicked it triggers prompt that takes number (1-100) and puts it in
    function canvasMaker().
    -Modify canvasMaker() so it only takes numbers from 1 to 100.
    -add min-height and min-width to canvas in css to fix overflow bug.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 const canvas = document.querySelector(".canvas");
+const canvasSizeButton = document.querySelector(".canvasSize");
 
-canvasMaker();
+canvasSizeButton.addEventListener("click", () => {
+  // Makes canvas with argument being determined by prompt
+  canvasMaker(prompt("Please enter number between 1 and 100"));
+});
+
+// Makes default canvas size 16x16
+canvasMaker(16);
 
 // Function that is checking if mouse is down (used with mousemove)
 function andMouseDown (e) {
   if (e.buttons == 1) {
     e.preventDefault();
-    color(e)
+    color(e);
   }
 }
 
@@ -16,13 +23,24 @@ function color (e) {
 }
 
 // Creates "pixels" in canvas, parameter is number of pixels in row/column
-function canvasMaker (n = 16) {
-  // Creates, alters and appends "pixels" in canvas (number of them is n x n)
-  for (let i = 0; i < (n * n); i++) {
-    let pixel = document.createElement("div");
-    pixel.setAttribute("style", `width: ${500/n}px; height: ${500/n}px;`);
-    pixel.addEventListener("mousemove", andMouseDown);
-    pixel.addEventListener("mousedown", color)
-    canvas.appendChild(pixel);
+function canvasMaker (n) {
+  if (n > 0 && n <= 100) {
+    canvasEraser();
+    // Creates, alters and appends "pixels" in canvas (number of them is n x n)
+    for (let i = 0; i < (n * n); i++) {
+      let pixel = document.createElement("div");
+      pixel.setAttribute("style", `width: ${500/n}px; height: ${500/n}px;`);
+      pixel.addEventListener("mousemove", andMouseDown);
+      pixel.addEventListener("mousedown", color)
+      canvas.appendChild(pixel);
+    }
+  } else {
+    alert("number invalid, try again.");
   }
+}
+
+// Removes all content from canvas with replaceChildren() API because it is 
+// faster than innerHTML and simpler than loop to remove all children one by one.
+function canvasEraser () {
+  canvas.replaceChildren();
 }
